77fc564c4a4b671793b391e92c47999905d7d374
hakyeop <127179902+hakyeop@users.noreply.github.com>
2024-08-04 23:01:33 +0900

    Create 자가진단1.c

---
+++ 9.배열1/자가진단1.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+    char a[10];
+    for(int i=0; i<10; i++){
+        scanf("%c ", &a[i]);
+    }
+    for(int i=0; i<10; i++){
+        printf("%c", a[i]);
+    }
+}
