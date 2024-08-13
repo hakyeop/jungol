ce72bfa2e5477f1c216be051699c3a6d61d073fb
hakyeop <127179902+hakyeop@users.noreply.github.com>
2024-08-13 18:03:14 +0900

    Create 연습문제6.c

---
+++ 9.배열1/연습문제6.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main()
+{
+    int a[10], b, c=0;
+    
+    for(int i=0; i<10; i++){
+        scanf("%d", &b);
+        if(b>c)
+            c=b;
+        a[i]=b;
+    }
+    printf("%d\n", c);
+}
