8112535ed16cf703eea217ebde5ac436dede36a0
hakyeop <127179902+hakyeop@users.noreply.github.com>
2024-08-28 13:44:51 +0900

    Create 자가진단2.c

---
+++ 배열2/자가진단2.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main()
+{
+    int a[10]={}, b;
+    while(1){
+        scanf("%d", &b);
+        if(b==0)
+            break;
+        a[b/10]++;
+    }
+    for(int i=0; i<10; i++){
+        if(a[i] > 0)
+            printf("%d : %d\n", i, a[i]);
+    }
+}
